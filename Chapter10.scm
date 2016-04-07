1804e05486afa9c6058b88766b22b0bb8079a562
Ibraheem Syed <ibraheem2811@gmail.com>
2016-04-06 20:19:20 -0700

    Complete Chapter10
    Lesson and exercise.

---
+++ Chapter10.scm
@@ -0,0 +1,47 @@
+#lang racket
+(require (planet dyoo/simply-scheme:2:2))
+
+
+;Lesson 
+
+;(define (square x) (* x x))
+
+;(define (hypotenuse x y)
+;(sqrt (+ (square x) (square y))))
+
+;(define (f x)
+;( 6))
+
+;(define pi 3.141592654)
+
+;(define song '( I am the walrus))
+
+;(se (if (vowel? (first wd)) 'an 'a)
+;wd
+;'is
+;(if (vowel? (first wd)) 'an 'a)
+;wd
+;'is
+;(if (vowel? (first wd)) 'an 'a)
+;wd)
+
+
+;Exercise
+
+;1. (let pi 3.14159
+;pie ’lemon meringue
+;se ’pi is pi ’but pie is pie)
+;(PI IS 3.14159 BUT PIE IS LEMON MERINGUE)
+
+;2 (let ((a (+ 4 7)))
+;(let ((b (* a 5)))
+;(+ a b)))
+
+;3. (define (roots a b c)
+;(let ((discriminant (sqrt (- (* b b) (* 4 a c))))
+;(minus-b (- b))
+;(two-a (* 2 a)))
+;(se (/ (+ minus-b discriminant) two-a)
+;(/ (- minus-b discriminant) two-a))))
+
+
